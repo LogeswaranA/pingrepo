d6ef796f62f38e96a0852e7679c5fb7f371e89f2
Logeswaran A <lokeshwaran.a82@gmail.com>
2022-04-04 16:38:20 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Node.js  utility
+   A easy to use utility for node.js to send ping requests
+
+##Installation
+
+    $ node app.js
---
+++ app.js
@@ -0,0 +1,40 @@
+const ping = require("ping");
+
+var ipList = ['104.152.209.58', '104.194.249.219', '1.2.3.4', '5.1.2.3', '104.152.209.58'];
+
+var dailyping = [];
+
+const pingIP = async () => {
+    for (i = 0; i < ipList.length; i++) {
+      const result = await ping.promise.probe(ipList[i], {
+        timeout: 3,
+        extra: ["-i", "2"],
+      });
+      var flag=0;
+      if(result.alive){
+        flag=1;
+      }
+      let obj = { "IPAddress": ipList[i], "Alive": result.alive, "flag":flag };
+      dailyping.push(obj);
+    }
+    console.log("dailyping", dailyping)
+}
+
+const calcReputation = async () => {
+  var ipList = ['104.152.209.58', '104.194.249.219', '1.2.3.4', '5.1.2.3', '104.152.209.58'];
+  var noofdays = [30, 25, 14, 5, 0];
+  var listofitems = []
+  for (i = 0; i < ipList.length; i++) {
+    const result = await ping.promise.probe(ipList[i], {
+      timeout: 3,
+      extra: ["-i", "2"],
+    });
+    var totaldays = noofdays[i] / 30;
+    var reputation = totaldays * 5;
+    let obj = { "IPAddress": ipList[i], "Alive": result.alive, "Reputation": reputation };
+    listofitems.push(obj);
+  }
+  console.log("listofitems", listofitems)
+}
+
+pingIP();
